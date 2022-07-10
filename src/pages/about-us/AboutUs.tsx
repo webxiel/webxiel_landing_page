13050a5df37a46ef09a6404f91804ccd1bb6dee5
Toochukwu Okolie <charlestoochukwu5@gmail.com>
2022-07-10 16:41:42 +0100

    feat: implement about-us page

--- src/pages/about-us/AboutUs.tsx
+++ src/pages/about-us/AboutUs.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import "./AboutUs.scss"
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/header/Header'
+import AboutWebxiel from './components/AboutWebxiel';
+import ourStory from '../../assets/images/our-story.png'
+import OurTeam from "./components/OurTeam";
+import { ReactComponent as Partners } from '../../assets/images/partners.svg'
+import { ReactComponent as Dots } from '../../assets/images/dots.svg' 
 
 const AboutUs = () => {
   const text = "We are a web based company looking at enabling business strive better online, using better web technologies";
@@ -8,7 +13,30 @@ const AboutUs = () => {
     <>
       <Header title='about us' text={text}/>
       <main className="about-us">
+        <section className="about-us__story">
+          <div className="about-us__story__container">
+            <article className="about-us__story__container__content">
+              <header className="about-us__story__container__content__header">
+                <h2 className="title">
+                  our story
+                </h2>
+              </header>
+              <p className="about-us__story__container__content__text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores minus esse harum eum deserunt. Officiis porro consequatur pariatur impedit cum nisi magni quasi minima quia blanditiis est itaque ut quam consequuntur tempora sed obcaecati eos, architecto ipsam temporibus nostrum eius.
+              </p>
+            </article>
 
+            <picture className="about-us__story__container__image">
+              <Dots />
+              <img src={ourStory} alt="webxiel staff" />
+            </picture>
+          </div>
+        </section>
+        <AboutWebxiel />
+        <OurTeam />
+        <section className="about-us__partners">
+          <Partners />
+        </section>
       </main>
       <Footer />
     </>
